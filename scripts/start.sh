6aab51bea22a809829c59849c090537b718a7cf7
Christoph Hummer <christophhummer@gmail.com>
2023-10-01 21:52:20 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -74,17 +74,4 @@ fi
 
 echo "Container ready, please open the console ${SSH_MESSAGE}to the container and go to ${DATA_DIR}"
 
-term_handler() {
-  kill -SIGTERM "$killpid"
-  wait "$killpid" -f 2>/dev/null
-  exit 143;
-}
-
-trap 'kill ${!}; term_handler' SIGTERM
-sleep infinity &
-killpid="$!"
-while true
-do
-  wait $killpid
-  exit 0;
-done
+sleep infinity
